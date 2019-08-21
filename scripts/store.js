472ed5a55a9c285c102bd61319746b5b2437178b
Quasar Wei <quasar.wei@outlook.com>
2019-08-21 08:45:18 -0700

    updated store with CRUD functions

--- scripts/store.js
+++ scripts/store.js
@@ -1,22 +1,62 @@
 'use strict';
-/* global cuid */
+/* global cuid, Item */
 
 // eslint-disable-next-line no-unused-vars
 const store = (function() {
 
+  // items array
   let items = [
     { id: cuid(), name: 'apples', checked: false },
     { id: cuid(), name: 'oranges', checked: false },
     { id: cuid(), name: 'milk', checked: true },
     { id: cuid(), name: 'bread', checked: false }
   ];
-  let hideCheckedItems = false;
-  let searchTerm = '';
+
+  let hideCheckedItems = false;        // hide checked items boolean
+  let searchTerm = '';                 // search term string
+
+  let findByID = function(id) {
+    this.items.find(item => id === item.id);
+  };
+
+  let addItem = function(name) {
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch(e) {
+      console.log(`${e.message}`);
+    }
+  };
+
+  let findAndToggleChecked = function(id) {
+    this.findByID(id).checked = !this.findByID(id).checked; 
+  };
+
+  let findAndUpdateName = function(id, newName) {
+    try {
+      Item.validateName(newName);
+      this.findByID(id).name = newName;
+    } catch(e) {
+      console.log(`Cannot update name: ${e.message}`);
+    }
+  };
+
+  let findAndDelete = function(id) {
+    const index = this.items.findIndex(item => item.id === id);
+    this.items.splice(index,1);
+  };
+
+
 
   return {
     items,
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    findByID,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   };
   
 }() );
