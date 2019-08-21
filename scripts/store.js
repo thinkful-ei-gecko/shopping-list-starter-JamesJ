31c78cfe145fde2db3396674b334d4e14467c8b4
Quasar Wei <quasar.wei@outlook.com>
2019-08-21 07:54:11 -0700

    1st part

---
+++ scripts/store.js
@@ -0,0 +1,22 @@
+'use strict';
+/* global cuid */
+
+// eslint-disable-next-line no-unused-vars
+const store = (function() {
+
+  let items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  let hideCheckedItems = false;
+  let searchTerm = '';
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm
+  };
+  
+}() );
